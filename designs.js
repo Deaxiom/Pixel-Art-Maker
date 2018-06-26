95e7233d3bb4a6296fc428f8fdf661fd62badd5b
Aaron usman <33053542+Deaxiom@users.noreply.github.com>
2018-06-26 16:26:33 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,63 +3,45 @@
 
 // When size is submitted by the user, call makeGrid()
 
+let height, width, color, reset;
 function makeGrid() {
-$(function (){
-
-// Select color input
-  var colorPicker = $('#colorPicker');
-// Select size input
-  var heightInput = $('#inputHeight');
-  var weightInput = $('#inputWeight');
-
-//select canvas
-  var canvas = $('#pixelCanvas');
-
-// When size is submitted by the user, call makeGrid()
-
-  function makeGrid() {
-    canvas.find('tbody').remove();
-
-  //submit button size changes to fit grid size
-    var gridRows = heightInput.val();
-    var gridCol = weightInput.val();
-
-  //tbody set to the table
-    canvas.append('<tbody></tbody>');
-
-    var canvasBody = canvas.find('tbody');
-
-  //drawing grid rows
-    for (var i = 0; i < gridRows; i++) {
-      canvasBody.append('<tr></tr>');
+    $("#pixel_canvas").html("");
+    height = $("#input_height").val();
+    width = $("#input_width").val();
+    if (height > 50 || width > 50 || height < 1 || width < 1) {
+        if (!error.classList.contains("error")) {
+            error.classList.toggle("error");
+            error.innerText = "the dimension has to be smaller than 50 and bigger than 0";
+            backUp();
+        }
+    } else {
+        error.innerText = "";
+        $("div").removeClass("error");
+        for (let x = 0; x < height; x++) {
+            $('#pixel_canvas').append('<tr></tr>');
+        }
+        for (let y = 0; y < width; y++) {
+            $('#pixel_canvas tr').each(function () {
+                $(this).append('<td></td>');
+            });
+        }
     }
-
-  //draw grid col
-    for (var i = 0; i < gridCol; i++) {
-      canvas.find('tr').append('<td class="transparent"></td>');
-    }
-  }
-
-//update grid when submit button clicked
-  $(document).ready(function(){
-    $('input[type="submit"]').on('click', function(e){
-      e.preventDefault();
-      makeGrid();
+}
+color = $('#colorPicker');
+$(document).on("mousedown", "tr td", function () {
+    let colorValue = color.val();
+    $(this).css('background-color', colorValue);
+    $('tr td').bind("mousemove", function () {
+        let colorValue = color.val();
+        $(this).css('background-color', colorValue);
+    }).mouseup(function() {
+        $('td').unbind('mousemove');
     });
-
-  //toggle grid color
-    $('#pixelCanvas').on('click', 'td', function(e){
-      var color = colorPicker.val();
-      var currentColor = $(this).css('background-color');
-      console.log(currentColor + ' color:' + color);
-
-      if ($(this).hasClass('transparent')) {
-        $(this).toggleClass('transparent');
-        $(this).css('background-color', color);
-      } else {
-        $(this).toggleClass('transparent');
-        $(this).css('background-color', 'transparent');
-      }
+    $('tr td').on('dblclick',function () {
+        $(this).css('background-color', "#FFFFFF")
     })
-  });
 });
+reset = $("#pixel_canvas").html();
+function backUp() {
+    $("#pixel_canvas").html(reset);
+}
